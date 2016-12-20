149e00b048bf82212efb72d59acca83ae0f90de4
Vincent B <vin100.beck@gmail.com>
2016-12-20 22:33:57 +0100

    Handle empty columns in the plyaground

---
+++ src/EmptyCard.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { AppRegistry, Animated, View, Image, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+
+/**
+ * Represent am empty card. It allows the user toc lick on an empty column
+ */
+class EmptyCard extends Component {
+    constructor(props) {
+        super(props);
+
+        // Handlers
+        this.handlePress = this.handlePress.bind(this);
+
+        // Style
+        this.style = this.getStyle();
+    }
+
+    /**
+     * Get the component style
+     * @returns {*}
+     */
+    getStyle() {
+        return StyleSheet.create({
+            view: {
+                width: 50,
+                height: 73
+            }
+        });
+    }
+
+    /**
+     * When you press
+     */
+    handlePress() {
+        this.props.onPress(null);
+    }
+
+    render() {
+        return (
+            <TouchableWithoutFeedback onPress={this.handlePress}>
+                <View style={[this.style.view, this.props.style]}/>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+export default EmptyCard;
