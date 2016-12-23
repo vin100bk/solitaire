9382175b3518c9e90dfd2b1a878b58ff350da4dc
Vincent B <vin100.beck@gmail.com>
2016-12-23 21:03:30 +0100

    CHange package name

--- android/app/src/main/java/com/solitaire/MainActivity.java
+++ android/app/src/main/java/com/solitaire/MainActivity.java
@@ -1,4 +1,4 @@
-package com.solitaire;
+package com.vin100bk.solitaire;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/solitaire/MainApplication.java
+++ android/app/src/main/java/com/solitaire/MainApplication.java
@@ -1,4 +1,4 @@
-package com.solitaire;
+package com.vin100bk.solitaire;
 
 import android.app.Application;
 import android.util.Log;
